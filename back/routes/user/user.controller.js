532796efab7991d976f36846a19899ca1283d3c0
pjunh1117 <pjunh1117@gmail.com>
2021-12-09 16:22:16 +0900

    마이페이지 back 추가

--- back/routes/user/user.controller.js
+++ back/routes/user/user.controller.js
@@ -3,6 +3,7 @@ const { off } = require("../../config/dbConfig");
 var router = express.Router();
 const UserService = require("./user.service");
 
+//회원가입
 router.post("/signUp", async (req, res) => {
   try {
     const result = await UserService.insertUser(req.body);
@@ -11,20 +12,10 @@ router.post("/signUp", async (req, res) => {
     return res.status(500).json({ status: 500, message: error });
   }
 });
-/* GET home page. */
-router.get("/memberlist", async (req, res) => {
-  try {
-    console.log("lol");
-    const result = await UserService.getUserList();
-    res.status(200).json({ status: 200, data: result, message: "Success" });
-  } catch (error) {
-    return res.status(500).json({ status: 500, message: error });
-  }
-});
 
+//로그인
 router.post("/login", async (req, res) => {
   try {
-    console.log("lol");
     const result = await UserService.checkUser(req.body);
     console.log(result);
     if (result === 0) {
@@ -45,12 +36,59 @@ router.post("/login", async (req, res) => {
   }
 });
 
-router.get("/my/:userNum", async (req, res) => {
-  //db 내계정 정보 가져오는 서비스
+//유저 계정 정보
+router.get("/myInfo/:userNum", async (req, res) => {
   try {
-    //파라미터를 사용
     let { userNum } = req.params;
     const result = await UserService.getMyInfo(userNum);
+    if (result.length === 0) {
+      res.status(200).json({ status: 200, message: "No data" });
+    } else {
+      res.status(200).json({ status: 200, data: result, message: "Success" });
+    }
+  } catch (error) {
+    return res.status(500).json({ status: 500, message: error });
+  }
+});
+
+//유저 즐겨찾기
+router.get("/myFavorite/:userNum", async (req, res) => {
+  try {
+    let { userNum } = req.params;
+    const result = await UserService.getMyFavorite(userNum);
+    res.status(200).json({ status: 200, data: result, message: "Success" });
+  } catch (error) {
+    return res.status(500).json({ status: 500, message: error });
+  }
+});
+
+//유저 활동
+router.get("/myPlay/:userNum", async (req, res) => {
+  try {
+    let { userNum } = req.params;
+    const result = await UserService.getMyPlay(userNum);
+    res.status(200).json({ status: 200, data: result, message: "Success" });
+  } catch (error) {
+    return res.status(500).json({ status: 500, message: error });
+  }
+});
+
+//내 등록 식당 조회
+router.get("/myRst/:userNum", async (req, res) => {
+  try {
+    let { userNum } = req.params;
+    const result = await UserService.getMyRst(userNum);
+    res.status(200).json({ status: 200, data: result, message: "Success" });
+  } catch (error) {
+    return res.status(500).json({ status: 500, message: error });
+  }
+});
+
+//유저 전체 리스트 가져오기
+router.get("/memberlist", async (req, res) => {
+  try {
+    console.log("lol");
+    const result = await UserService.getUserList();
     res.status(200).json({ status: 200, data: result, message: "Success" });
   } catch (error) {
     return res.status(500).json({ status: 500, message: error });
