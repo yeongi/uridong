0aef1604c91c2e5f6d7426360f3155a0b53b9651
pjunh1117 <pjunh1117@gmail.com>
2021-12-09 20:25:33 +0900

    주석처리

--- back/routes/coupon/coupon.controller.js
+++ back/routes/coupon/coupon.controller.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const CouponService = require("./coupon.service");
 
+//내 쿠폰 확인
 router.get("/hav/:userNum", async (req, res) => {
   try {
     const { userNum } = req.params;
