9c88cd40b29b9c3360876bfa78772def3e25d21c
J-mini0000 <woals0114@nate.com>
2021-12-10 21:42:42 +0900

    asd

--- back/routes/ad/Advertise.controller.js
+++ back/routes/ad/Advertise.controller.js
@@ -16,12 +16,40 @@ router.get("/adlist", async (req, res) => {
     return res.status(500).json({ status: 500, message: error });
   }
 });
-
+//유효한 광고 리스트 가져오기
 router.get("/Validadlist", async (req, res) => {
-
+try {
+    console.log("유효한 광고목록 가져오기");
+    const result = await AdService.Validadlist();
+    res.status(200).json({ status: 200, data: result, message: "Success" });
+  } catch (error) {
+    return res.status(500).json({ status: 500, message: error });
+  }
+});
+//유효하지 않은 광고리스트 가져오기
+router.get("/inValidAdlist", async (req, res) => {
   try {
-    console.log("광고목록 가져오기");
-    const result = await AdService.getAllList();
+    console.log("유효하지않은 광고목록 가져오기");
+    const result = await AdService.inValidAdlist();
+    res.status(200).json({ status: 200, data: result, message: "Success" });
+  } catch (error) {
+    return res.status(500).json({ status: 500, message: error });
+  }
+});
+//광고 등록 신청하기
+router.post("/AdReApp", async (req, res) => {
+  try {
+    const result = await RstService.registappAd();
+    res.status(200).json({ status: 200, data: result, message: "Success" });
+  } catch (error) {
+    return res.status(500).json({ status: 500, message: error });
+  }
+});
+//광고 승인하기
+router.get("/accAd/:ADnum", async (req, res) => {
+  let { ADnum } = req.params;
+  try {
+    const result = await RstService.acceptAd(ADnum);
     res.status(200).json({ status: 200, data: result, message: "Success" });
   } catch (error) {
     return res.status(500).json({ status: 500, message: error });
