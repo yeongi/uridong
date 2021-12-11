af2d3159effe180227b9d64d0db8dbbb2c87df19
J-mini0000 <woals0114@nate.com>
2021-12-11 16:24:31 +0900

    as

--- back/routes/favorite/favorite.controller.js
+++ back/routes/favorite/favorite.controller.js
@@ -12,4 +12,27 @@ router.post("/add", async (req, res) => {
   }
 });
 
+//단골손님 
+router.get("/patron/:rstnum", async (req, res) => {
+  let { rstnum } = req.params;
+  try {
+    const result = await FavService.getpatronList(rstnum);
+    res.status(200).json({ status: 200, data: result, message: "Success" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ status: 500, message: error });
+  }
+});
+
+//한달 이상 리뷰안쓴 단골
+router.get("/patron/:rstnum", async (req, res) => {
+  let { rstnum } = req.params;
+  try {
+    const result = await FavService.getOneMonthpatronList(rstnum);
+    res.status(200).json({ status: 200, data: result, message: "Success" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ status: 500, message: error });
+  }
+});
 module.exports = router;
--- back/routes/favorite/favorite.service.js
+++ back/routes/favorite/favorite.service.js
@@ -18,4 +18,32 @@ module.exports = {
       throw error;
     }
   },
+  getpatronList: async (rstnum) => {
+    try {
+      const conn = await pool.getConnection();
+      const query = `select * form restaurant inner join favorite on restaurant.rst_num= favorite.rst_num where restaurant.rst_num=? and favorite.patron_yn=true`;
+      const [{ affectRows: result }] = await conn.query(query, [rstnum]);
+      conn.release();
+      return result;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  },
+
+  getOneMonthpatronList: async (rstnum) => {
+    try {
+      const conn = await pool.getConnection();
+      const query = `select member_name, member_email
+      from member inner join favorite on member.member_num = favorite.member_num
+      where rst_num=? and last_review_date >= date_add(now(),interval -1 month) 
+      and patron_yn=true;`;
+      const [{ affectRows: result }] = await conn.query(query, [rstnum]);
+      conn.release();
+      return result;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  },
 };
