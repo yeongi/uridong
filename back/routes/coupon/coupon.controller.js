07061418cd0b1be3a84c1240eb6221d1dbd75cac
pjunh1117 <pjunh1117@gmail.com>
2021-12-11 20:12:48 +0900

    쿠폰 생성 (입력 값 일부는 하드코딩으로 임시설정)

--- back/routes/coupon/coupon.controller.js
+++ back/routes/coupon/coupon.controller.js
@@ -23,4 +23,24 @@ router.post("/havRst", async (req, res) => {
   }
 });
 
+// //쿠폰 발급화면 보기 -> 메뉴 num을 가져와 메뉴이름 전체 조회
+// router.get("/madePage", async (req, res) => {
+//   try {
+//     const result = await CouponService.lookMakeCoupon(req.body);
+//     res.status(200).json({ status: 200, data: result, message: "Success" });
+//   } catch (error) {
+//     return res.status(500).json({ status: 500, message: error });
+//   }
+// });
+
+//쿠폰 발급
+router.post("/made", async (req, res) => {
+  try {
+    const result = await CouponService.makeCoupon(req.body);
+    res.status(200).json({ status: 200, data: result, message: "Success" });
+  } catch (error) {
+    return res.status(500).json({ status: 500, message: error });
+  }
+});
+
 module.exports = router;
--- back/routes/coupon/coupon.service.js
+++ back/routes/coupon/coupon.service.js
@@ -30,4 +30,53 @@ module.exports = {
       throw error;
     }
   },
+  lookMakeCoupon: async (info) => {
+    try {
+      const { rst_num } = info;
+      const conn = await pool.getConnection();
+      const query = `SELECT *
+      FROM restaurant_menu where rst_num = ?;`;
+      const [result] = await conn.query(query, [rst_num]);
+      conn.release();
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  },
+  makeCoupon: async (couponInfo) => {
+    try {
+      const {
+        rst_num,
+        menu_num, //임시로 하드코딩
+        coupon_kind,
+        coupon_name,
+        menu_name,
+        pic_file_path, //임시로 null처리
+        use_possible_man,
+        use_possible_time,
+      } = couponInfo; //식당쿠폰과 메뉴 조인
+      const conn = await pool.getConnection();
+      const query = `INSERT INTO restaurant_coupon (rst_num, menu_num,
+        coupon_kind,
+        coupon_name,
+        menu_name,
+        pic_file_path,
+        use_possible_man,
+        use_possible_time,) VALUES (?,4,?,?,?,null,?,?);`;
+      const [result] = await conn.query(query, [
+        rst_num,
+        // menu_num
+        coupon_kind,
+        coupon_name,
+        menu_name,
+        // pic_file_path,
+        use_possible_man,
+        use_possible_time,
+      ]);
+      conn.release();
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  },
 };
